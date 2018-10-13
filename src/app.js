abefea9be56bb9cb1a2e4a5886f0d5510dcc11e6
Adams Alves <alves.adams@gmail.com>
2018-10-13 17:16:07 -0300

    init with boilerplate

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return <h1>Aplicação com Module Hot Loader</h1>
+  }
+})
+
+export default App
