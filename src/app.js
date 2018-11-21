8d5364a4e4b5ffebade56cf11020e06aa2f05533
Adams Alves <alves.adams@gmail.com>
2018-11-21 18:05:19 -0200

    exp avents

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ import Square from './square'
 class App extends Component {
   render () {
     return (
-      <div className='container'>
-        {['blue', 'red', 'green'].map((square, index) => (
-          <Square key={index} color={square} />
-        ))}
+      <div className='container' onClick={(e) => {
+        alert('oi')
+      }}>
+        <Square />
       </div>
     )
   }
