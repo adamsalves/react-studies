463dd7a353d07f2d639b66d59e220d635b41c9f9
Adams Alves <alves.adams@gmail.com>
2018-11-30 19:06:08 -0200

    add async setState

--- src/app.js
+++ src/app.js
@@ -3,12 +3,33 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      checked: false,
+      showContent: false
+    }
+  }
   render () {
     return (
       <div>
-        <form>
-          
-        </form>        
+        <label>
+          <input
+            type='checkbox'
+            checked={this.state.checked}
+            onChange={() => {
+              this.setState({
+                checked: !this.state.checked
+              }, () => {
+                this.setState({
+                  showContent: this.state.checked
+                })
+              })
+            }}
+          /> Mostrar conteúdo
+        </label>
+
+        {this.state.showContent && <div>Olha eu aqui</div>}
       </div>
     )
   }
