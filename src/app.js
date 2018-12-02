9d6709196eec482a0f6ea204a53e43350fd7a13a
Adams Alves <alves.adams@gmail.com>
2018-12-02 16:53:32 -0200

    organizing the state information

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import React, { Component } from 'react'
 import AppContent from './components/app-content'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      userinfo: {
+        username: 'Adams Alves',
+        repos: 12,
+        followers: 10,
+        following: 10
+      },
+      repos: [{
+        name: 'Repos',
+        link: '#'
+      }],
+      starred: [{
+        name: 'Repo',
+        link: '#'
+      }]
+    }
+  }
   render () {
-    return <AppContent />
+    return <AppContent
+      userinfo={this.state.userinfo}
+      repos={this.state.repos}
+      starred={this.state.starred}
+    />
   }
 }
 
--- src/components/app-content.js
+++ src/components/app-content.js
@@ -1,33 +1,39 @@
 'use strict'
 
-import React from 'react'
+import React, { PropTypes } from 'react'
 import Search from './search'
 import UserInfo from './user-info'
 import Actions from './actions'
 import Repos from './repos'
 
-const AppContent = () => (
+const AppContent = ({ userinfo, repos, starred }) => (
   <div className='app'>
     <Search />
-    <UserInfo />
-    <Actions />
-    <Repos
-      className='repos'
-      title='Repositórios:'
-      repos={[{
-        name: 'Nome do repositório',
-        link: '#'
-      }]}
-    />
-    <Repos
-      className='starred'
-      title='Favoritos:'
-      repos={[{
-        name: 'Nome do repositório',
-        link: '#'
-      }]}
-    />
+    {!!userinfo && <UserInfo userinfo={userinfo} />}
+    {!!userinfo && <Actions />}
+
+    {!!repos.length &&
+      <Repos
+        className='repos'
+        title='Repositórios:'
+        repos={repos}
+      />
+    }
+
+    {!!starred.length &&
+      <Repos
+        className='starred'
+        title='Favoritos:'
+        repos={starred}
+      />
+    }
   </div>
 )
+
+AppContent.PropTypes = {
+  userinfo: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  starred: PropTypes.object.isRequired
+}
 
 export default AppContent
