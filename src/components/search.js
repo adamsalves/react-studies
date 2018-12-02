53e18da81b0644e96c01ddc98491b55f75cb6cd8
Adams Alves <alves.adams@gmail.com>
2018-12-02 20:37:25 -0200

    add ajax lib

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,25 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
   <div className='search'>
-    <input type='search' placeholder='Digite o nome do usuário no Github' />
+    <input
+      type='search'
+      placeholder='Digite o nome do usuário no Github'
+      onKeyUp={(e) => {
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+        if (keyCode === ENTER) {
+          ajax().get(`https://api.github.com/users/${value}`)
+            .then((result) => {
+              console.log(result)
+            })
+        }
+      }}
+    />
   </div>
 )
 
