dc1a87ab5d8ed0ff001edf9ab87e634c117c3167
Adams Alves <alves.adams@gmail.com>
2018-12-02 22:38:51 -0200

    repo data

--- src/app.js
+++ src/app.js
@@ -2,34 +2,45 @@
 
 import React, { Component } from 'react'
 import AppContent from './components/app-content'
+import ajax from '@fdaciuk/ajax'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      userinfo: {
-        username: 'Adams Alves',
-        photo: 'https://avatars1.githubusercontent.com/u/2552756?v=4',
-        login: 'adamsalves',
-        repos: 12,
-        followers: 10,
-        following: 10
-      },
-      repos: [{
-        name: 'Repos',
-        link: '#'
-      }],
-      starred: [{
-        name: 'Repo',
-        link: '#'
-      }]
+      userinfo: null,
+      repos: [],
+      starred: []
     }
   }
+
+  handleSearch (e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+    if (keyCode === ENTER) {
+      ajax().get(`https://api.github.com/users/${value}`)
+        .then((result) => {
+          this.setState({
+            userinfo: {
+              username: result.name,
+              photo: result.avatar_url,
+              login: result.login,
+              repos: result.public_repos,
+              followers: result.followers,
+              following: result.following
+            }
+          })
+        })
+    }
+  }
+
   render () {
     return <AppContent
       userinfo={this.state.userinfo}
       repos={this.state.repos}
       starred={this.state.starred}
+      handleSearch={(e) => this.handleSearch(e)}
     />
   }
 }
